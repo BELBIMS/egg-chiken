a76092274edb5ba48059ad506d45ed976c74c75e
Душин Максим <60781555+BELBIMS@users.noreply.github.com>
2023-12-18 12:08:39 +0300

    Update Program.cs

--- EggOrChicken/EggOrChicken/Program.cs
+++ EggOrChicken/EggOrChicken/Program.cs
@@ -1,25 +1,29 @@
-﻿internal class Program
+internal class Program
 {
     static void MyThreadChicken()
     {
         for (int i = 0; i < 100; i++)
-            Console.WriteLine("Курица");
+        {
+            Console.WriteLine("Курица"); // Выводит слово "Курица" 100 раз
+        }
     }
 
     static void MyThreadAnswer(Thread chicken)
     {
-        Thread.Sleep(10);
-        bool ans = chicken.IsAlive;
-        Console.WriteLine(ans ? "Спор Окончен. Победила Курица." : "Спор окончен. Победило Яйцо.");
+        Thread.Sleep(10); // Приостанавливает выполнение потока на 10 миллисекунд
+        bool ans = chicken.IsAlive; // Проверяет, активен ли поток "chicken"
+        Console.WriteLine(ans ? "Спор Окончен. Победила Курица." : "Спор окончен. Победило Яйцо."); // Выводит результат спора 
     }
-    private static void Main(string[] args)
+    private static void Main(string args)
     {
-        Thread chicken = new(MyThreadChicken);
-        chicken.Start();
+        Thread chicken = new(ThreadChicken); // Создаёт новый поток "chicken" и передаёт метод ThreadChicken как делегат
+        chicken.Start(); // Запускает поток "chicken"
 
         for (int i = 0; i < 100; i++)
-            Console.WriteLine("Яйцо");
+        {
+            Console.WriteLine("Яйцо"); // Выводит слово "Яйцо" 100 раз
+        }
 
-        MyThreadAnswer(chicken);
+        MyThreadAnswer(chicken); // Выполняет метод MyThreadAnswer, передавая поток "chicken" в качестве аргумента
     }
 }
